88dbadf3a7e68c7ace8b88bc9da30bfbd5061fb2
abdikhadir <abdikhadirroble@hotmail.com>
2019-09-06 09:36:51 +0100

    Update gitClass.java

--- gitClass.java
+++ gitClass.java
@@ -2,7 +2,7 @@ public class gitClass [
 
 	public static void main (String[] args) {
 
-		System.out.println("This is the git project");
+		System.out.println("This is the git training project");
 	}
 
 }
